d4744aa4b1db7acedc5a51d01c6c88019dfe47cb
miya0001 <miya@wpist.me>
2016-10-14 01:08:16 +0900

    confirm message and more

--- cli.php
+++ cli.php
@@ -49,7 +49,8 @@ class WP_CLI_Scaffold_Movefile extends WP_CLI_Command
 	 *     wp scaffold movefile --movefile=/path/to/Movefile
 	 *
 	 */
-	function __invoke( $args, $assoc_args ) {
+	function __invoke( $args, $assoc_args )
+	{
 		$vars = array(
 			'site_url' => site_url(),
 			'wordpress_path' => WP_CLI::get_runner()->config['path'],
@@ -65,7 +66,58 @@ class WP_CLI_Scaffold_Movefile extends WP_CLI_Command
 			$vars
 		);
 
-		WP_CLI::line( $movefile );
+		if ( empty( $args[0] ) ) {
+			$filename = getcwd() . "/Movefile";
+		} else {
+			$filename = $args[0];
+		}
+
+		$force = \WP_CLI\Utils\get_flag_value( $assoc_args, 'force' );
+		$result = $this->create_file( $filename, $movefile, $force );
+
+		if ( $result ) {
+			WP_CLI::success( $filename );
+		}
+	}
+
+	private function create_file( $filename, $contents, $force )
+	{
+		$wp_filesystem = $this->init_wp_filesystem();
+
+		$should_write_file = $this->prompt_if_files_will_be_overwritten( $filename, $force );
+		if ( $should_write_file ) {
+			$wp_filesystem->mkdir( dirname( $filename ) );
+			if ( ! $wp_filesystem->put_contents( $filename, $contents ) ) {
+				WP_CLI::error( "Error creating file: $filename" );
+			}
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Initialize WP Filesystem
+	 */
+	private function init_wp_filesystem()
+	{
+		global $wp_filesystem;
+		WP_Filesystem();
+		return $wp_filesystem;
+	}
+
+	private function prompt_if_files_will_be_overwritten( $filename, $force )
+	{
+		$should_write_file = false;
+		if ( ! file_exists( $filename ) ) {
+			return true;
+		}
+		WP_CLI::warning( 'File already exists.' );
+		if ( ! $force ) {
+			$should_write_file = cli\confirm( 'Do you want to overwrite', false );
+		}
+
+		return $should_write_file;
 	}
 }
 
